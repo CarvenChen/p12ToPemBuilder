6a5d42f1f7aef003b94db9b179cf536ced99f53f
chenhh116 <chenhh116@chinaunicom.cn>
2024-05-08 11:36:08 +0800

    appdelegate

--- CCPemBuilder/CCPemBuilder/AppDelegate.m
+++ CCPemBuilder/CCPemBuilder/AppDelegate.m
@@ -17,6 +17,8 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
     NSLog(@"");
+    NSLog(@"");
+    NSLog(@"");
 }
 
 
